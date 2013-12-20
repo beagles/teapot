ae3ddd64d91884f91fc93afb425d7f2a842ec5a6
Brent Eagles <beagles@redhat.com>
2013-12-20 13:44:42 -0330

    Some fixes and renames
    Renamed a couple of files to make them a bit more sensible from a "running
    perspective" and made the redirection of output a bit more sensible.

--- iso_run.sh
+++ iso_run.sh
@@ -8,7 +8,10 @@ breakonexit=$2
 
 if [ -e "$testgroup.tests.txt" ];
 then
-    bash $HOME/cleanups.sh
+    msg=`date`
+    echo "====================================================================" >> $testgroup.results.txt
+    echo "New run $msg" >> $testgroup.results.txt
+    bash -x teapotcleanups.sh >> $testgroup.results.txt
     cat "$testgroup.tests.txt" | while read testname rest;
     do
 	testr run $testname | tee -a $testgroup.results.txt | tee $testname.result.txt
@@ -17,10 +20,10 @@ then
 	then
 	    grep -q "$testname" failing.tests.txt
 	    [ $? -eq 1 ] && echo "$testname" >> failing.tests.txt
-	    bash $HOME/cleanups.sh >> $testname.result.txt
+	    bash -X teapotcleanups.sh | tee -a $testname.result.txt
 	    [ -n "$breakonexit" ] && [ "$breakonexit" == "yes" ] && exit
 	else
-	    bash $HOME/cleanups.sh | tee -a $testname.result.txt
+	    bash -x teapotcleanups.sh | tee -a $testname.result.txt
 	fi
     done
 else
